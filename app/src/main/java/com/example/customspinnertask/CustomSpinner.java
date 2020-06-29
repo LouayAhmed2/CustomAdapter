58350ea34250d2d656712eade6246fbc992c0cc1
LouayAhmed2 <65208442+LouayAhmed2@users.noreply.github.com>
2020-06-29 20:14:49 +0200

    -add CustomSpinner class extends AppCompatSpinner

---
+++ app/src/main/java/com/example/customspinnertask/CustomSpinner.java
@@ -0,0 +1,20 @@
+package com.example.customspinnertask;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatSpinner;
+
+public class CustomSpinner extends AppCompatSpinner {
+    public CustomSpinner(Context context) {
+        super(context);
+    }
+
+    public CustomSpinner(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomSpinner(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
