1d701a0bb7622b6f5d590fd80af56ea5c88b8088
LouayAhmed2 <65208442+LouayAhmed2@users.noreply.github.com>
2020-06-29 21:42:19 +0200

    -create class user which implement IDisplay interface  -create String method user name and constructor

--- app/src/main/java/com/example/customspinnertask/MainActivity.java
+++ app/src/main/java/com/example/customspinnertask/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.customspinnertask;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    CustomSpinner customSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        customSpinner = findViewById(R.id.spinner);
+        List<IDisplay> data = getTestData();
+        customSpinner.setData(data);
+
+
+    }
+
+    public List<IDisplay> getTestData() {
+        List<IDisplay> data = new ArrayList<>();
+        for (int i = 0; i < 5; i++)
+            data.add(new User("userNum " + i));
+
+        return data;
     }
 }
---
+++ app/src/main/java/com/example/customspinnertask/User.java
@@ -0,0 +1,14 @@
+package com.example.customspinnertask;
+
+public class User implements IDisplay {
+    String Username;
+
+    public User(String username) {
+        Username = username;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}
