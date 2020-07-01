06d0c3358eb087ffc566e25ff4d624e068cd98df
LouayAhmed2 <65208442+LouayAhmed2@users.noreply.github.com>
2020-07-01 02:34:49 +0200

    - Add some Documentation   -fix some pugs

--- app/src/main/java/com/example/customspinnertask/CustomSpinner.java
+++ app/src/main/java/com/example/customspinnertask/CustomSpinner.java
@@ -43,20 +43,38 @@ public class CustomSpinner extends AppCompatSpinner {
             setBackgroundColor(backgroundColor);
             setTextAlignment(TextAlignment);
         } finally {
-
             a.recycle();
-
         }
-
-
     }
 
-
+    /**
+     * Set the text alignment.
+     *
+     * @param textAlignment The text alignment to set. Should be one of
+     *                      {@link #TEXT_ALIGNMENT_INHERIT},
+     *                      {@link #TEXT_ALIGNMENT_GRAVITY},
+     *                      {@link #TEXT_ALIGNMENT_CENTER},
+     *                      {@link #TEXT_ALIGNMENT_TEXT_START},
+     *                      {@link #TEXT_ALIGNMENT_TEXT_END},
+     *                      {@link #TEXT_ALIGNMENT_VIEW_START},
+     *                      {@link #TEXT_ALIGNMENT_VIEW_END}
+     *                      Resolution will be done if the value is set to TEXT_ALIGNMENT_INHERIT. The resolution
+     *                      proceeds up the parent chain of the view to get the value. If there is no parent, then it
+     *                      will return the default {@link #TEXT_ALIGNMENT_GRAVITY}.
+     * @attr ref android.R.styleable#View_textAlignment
+     */
     @Override
     public void setTextAlignment(int textAlignment) {
         super.setTextAlignment(textAlignment);
     }
 
+
+    /**
+     * Set the background to a given resource. The resource should refer to
+     * a Drawable object or 0 to remove the background.
+     * @param resId The identifier of the resource.
+     * @attr ref android.R.styleable#View_background
+     */
     @Override
     public void setBackgroundResource(int resId) {
         super.setBackgroundResource(resId);
--- app/src/main/java/com/example/customspinnertask/CustomSpinnerAdapter.java
+++ app/src/main/java/com/example/customspinnertask/CustomSpinnerAdapter.java
@@ -22,10 +22,23 @@ public class CustomSpinnerAdapter extends BaseAdapter {
         this.onSpinnerItemClickListener = onSpinnerItemClickListener;
     }
 
+    /**
+     * How many items are in the data set represented by this Adapter.
+     *
+     * @return Count of list.size.
+     */
     @Override
     public int getCount() {
         return list.size();
     }
+
+    /**
+     * Get the data item associated with the specified position in the IDisplay set.
+     *
+     * @param position Position of the item whose data we want within the adapter's
+     *                 data set.
+     * @return The data at the specified position.
+     */
 
     @Override
     public IDisplay getItem(int position) {
@@ -37,7 +50,24 @@ public class CustomSpinnerAdapter extends BaseAdapter {
         return position;
     }
 
-
+    /**
+     * Get a View that displays the data at the specified position in the data set. You can either
+     * create a View manually or inflate it from an XML layout file. When the View is inflated, the
+     * parent View (GridView, ListView...) will apply default layout parameters unless you use
+     * {@link android.view.LayoutInflater#inflate(int, android.view.ViewGroup, boolean)}
+     * to specify a root view and to prevent attachment to the root.
+     *
+     * @param position The position of the item within the adapter's data set of the item whose view
+     *        we want.
+     * @param convertView The old view to reuse, if possible. Note: You should check that this view
+     *        is non-null and of an appropriate type before using. If it is not possible to convert
+     *        this view to display the correct data, this method can create a new view.
+     *        Heterogeneous lists can specify their number of view types, so that this View is
+     *        always of the right type (see {@link #getViewTypeCount()} and
+     *        {@link #getItemViewType(int)}).
+     * @param parent The parent that this view will eventually be attached to
+     * @return A View corresponding to the data at the specified position.
+     */
     public View getView(final int position, View convertView, final ViewGroup parent) {
         ViewHolder viewHolder = null;
         if (convertView == null) {
@@ -53,6 +83,31 @@ public class CustomSpinnerAdapter extends BaseAdapter {
         if (iDisplay != null) {
             viewHolder.textView.setText(iDisplay.getName());
         }
+        return convertView;
+    }
+
+    @Override
+    public View getDropDownView(final int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder = null;
+        if (convertView == null) {
+
+            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.item_spinner, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.textView = convertView.findViewById(R.id.text_view_spinner);
+            convertView.setTag(viewHolder);
+        } else viewHolder = (ViewHolder) convertView.getTag();
+        final IDisplay iDisplay = getItem(position);
+        if (iDisplay != null) {
+            viewHolder.textView.setText(iDisplay.getName());
+        }
+        if (onSpinnerItemClickListener != null)
+            convertView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onSpinnerItemClickListener.onItemSelected(iDisplay, position);
+                }
+            });
         return convertView;
     }
 
